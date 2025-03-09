6e5d1cf22eb4dd4ec3abb39f716db6949d266093
Steven N <steven.hgng@gmail.com>
2025-03-09 01:01:33 +0000

    first livewire

---
+++ app/Livewire/QuestionaireContent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class QuestionaireContent extends Component
+{
+    public function render()
+    {
+        return view('livewire.questionaire-content');
+    }
+}
